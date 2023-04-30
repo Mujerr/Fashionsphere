034ef8255f0e9e38d4133591b6e9e5bd85e18c53
mujerr <martalouwu@gmail.com>
2023-04-30 18:19:22 +0200

    CREACION DE COMPONENTES INICIACLES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './generales/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarseComponent } from './componentes/registrarse/registrarse.component';
+import { CarritoComponent } from './componentes/carrito/carrito.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    IniciarSesionComponent,
+    RegistrarseComponent,
+    CarritoComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,

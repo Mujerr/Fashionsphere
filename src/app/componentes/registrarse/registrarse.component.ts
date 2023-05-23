ff0fc865c22a2b3ed6dd1380359a21d65ef7c060
mujerr <martalouwu@gmail.com>
2023-05-23 21:45:23 +0200

    COMPELTO NAVBAR + mejora registro

--- src/app/componentes/registrarse/registrarse.component.ts
+++ src/app/componentes/registrarse/registrarse.component.ts
@@ -4,6 +4,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { GoogleAuthProvider } from 'firebase/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { finalize } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registrarse',
@@ -18,9 +20,10 @@ export class RegistrarseComponent {
   nombre: string = '';
   apellidos:string = '';
   username: string = '';
+  profilePicture:string = '';
   selectedProfilePicture: File | null = null;
 
-  constructor(private afAuth: AngularFireAuth,   private firestore: AngularFirestore, private storage: AngularFireStorage) {}
+  constructor(private afAuth: AngularFireAuth,   private firestore: AngularFirestore, private storage: AngularFireStorage, private router:Router) {}
 
   onFileSelected(event: any) {
     this.selectedProfilePicture = event.target.files[0];
@@ -37,19 +40,24 @@ export class RegistrarseComponent {
       const nombre = this.nombre;
       const apellidos = this.apellidos;
       const username = this.username;
+      const profilePicture = this.profilePicture;
+
       // Aquí puedes realizar el registro con el email y la contraseña en Firebase
       // Puedes utilizar el servicio AngularFireAuth para esto
       this.afAuth.createUserWithEmailAndPassword(email, password)
         .then((credential) => {
           const user = credential.user; // Obtener el usuario autenticado
-  
           if (user) {
             // Guardar datos adicionales del usuario en la base de datos
             const userData = {
               email: email,
               phone: phone,
               address: address,
-              profilePicture: '' 
+              profilePicture: '' ,
+              nombre:nombre,
+              apellidos:apellidos,
+
+              username:username
             };
   
             // Obtener la referencia al documento del usuario en la colección 'users'
@@ -58,16 +66,13 @@ export class RegistrarseComponent {
             // Guardar los datos adicionales en el documento del usuario
             userRef.set(userData)
               .then(() => {
-                console.log("Datos adicionales del usuario guardados correctamente");
-                // Aquí puedes redirigir al usuario a otra página o realizar otras acciones
+                this.router.navigate(['/home']);
               })
-              .catch((error) => {
-                console.log("Error al guardar datos adicionales del usuario:", error);
-              });
+              .catch((error) => { this.errorRegistro(); });
 
     if (this.selectedProfilePicture) {
 
-      const filePath = `users/${user.uid}/${this.selectedProfilePicture.name}`;
+      const filePath = `users/${user.uid}/${this.selectedProfilePicture.name.includes(user.uid)}`;
       const fileRef = this.storage.ref(filePath);
       const uploadTask = this.storage.upload(filePath, this.selectedProfilePicture);
   
@@ -80,12 +85,10 @@ export class RegistrarseComponent {
             // Guardar los datos adicionales del usuario en la base de datos
             userRef.set(userData)
               .then(() => {
-                console.log("Datos adicionales del usuario guardados correctamente");
+                this.router.navigate(['/home']);
                 // Aquí puedes redirigir al usuario a otra página o realizar otras acciones
               })
-              .catch((error) => {
-                console.log("Error al guardar datos adicionales del usuario:", error);
-              });
+              .catch((error) => { this.errorRegistro(); });
           });
         })
       ).subscribe();
@@ -93,21 +96,16 @@ export class RegistrarseComponent {
       // Si no se seleccionó ninguna imagen, guardar los datos adicionales del usuario sin la URL de la foto de perfil
       userRef.set(userData)
         .then(() => {
-          console.log("Datos adicionales del usuario guardados correctamente");
+          this.router.navigate(['/home']);
+
           // Aquí puedes redirigir al usuario a otra página o realizar otras acciones
         })
-        .catch((error) => {
-          console.log("Error al guardar datos adicionales del usuario:", error);
-        });
+        .catch((error) => { this.errorRegistro(); });
 
     }
-    } else {
-      console.log("No se ha encontrado el usuario autenticado");
-    }
+    } else { this.errorRegistro(); }
   })
-  .catch((error) => {
-    console.log("Error al registrar usuario con correo electrónico y contraseña:", error);
-  });
+  .catch((error) => { this.errorRegistro(); });
   
     // Reiniciar los campos del formulario después del registro
     registerForm.reset();
@@ -120,23 +118,25 @@ export class RegistrarseComponent {
       .then((result) => {
         // Obtener el usuario autenticado
         const user = result.user;
-  
+        const profilePicture = user?.photoURL
         if (user) {
           // Guardar datos adicionales del usuario en la base de datos
           const userData = {
             email: user.email,
             phone: this.phone,
-            address: this.address
+            address: this.address,
+            nombre:this.nombre,
+            apellidos:this.apellidos,
+            username:user.email,
+            profilePicture: profilePicture
           };
   
           this.firestore.collection('users').doc(user.uid).set(userData)
             .then(() => {
-              console.log("Datos adicionales del usuario guardados correctamente");
+              this.router.navigate(['/home']);
               // Aquí puedes redirigir al usuario a otra página o realizar otras acciones
             })
-            .catch((error) => {
-              console.log("Error al guardar datos adicionales del usuario:", error);
-            });
+            .catch((error) => { this.errorRegistro(); });
   
           // Reiniciar los campos del formulario después del registro
           this.email = '';
@@ -144,12 +144,17 @@ export class RegistrarseComponent {
           this.phone = '';
           this.address = '';
         } else {
-          console.log("No se ha encontrado el usuario autenticado");
+          this.errorRegistro();
         }
       })
       .catch((error) => {
-        console.log("Error al registrarse con Google:", error);
+        this.errorRegistro();
       });
   }
-  
+  errorRegistro(){
+    Swal.fire({
+      icon: 'error',
+      title: 'no se ha podido registrar el usuario',
+    }) 
+  }
 }
--- src/app/generales/navbar/navbar.component.ts
+++ src/app/generales/navbar/navbar.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import firebase from 'firebase/compat/app';
+import { User } from 'firebase/auth';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -11,22 +16,79 @@ export class NavbarComponent implements OnInit {
   selectedLang = 'es';
   userLoggedIn: boolean | undefined;
 
-  constructor(public translate: TranslateService, private afAuth: AngularFireAuth, private router: Router) {
-    translate.setDefaultLang('es'); 
-    translate.use('es'); 
+  user: User | null = null;
+  userName: string | null = '';
+  userPhoto: string | null = '';
+
+  constructor(
+    public translate: TranslateService,
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private firestore: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
+    translate.setDefaultLang('es');
+    translate.use('es');
   }
-  ngOnInit(){
+
+  ngOnInit() {
     this.afAuth.authState.subscribe(user => {
       this.userLoggedIn = user ? true : false;
+      this.user = user as User;
+      if (user) {
+        this.getUserData(user.uid);
+      } else {
+        this.userName = null;
+        this.userPhoto = null;
+      }
     });
   }
+
+  getUserData(uid: string) {
+    this.firestore.collection('users').doc(uid).get().subscribe(
+      (userDoc) => {
+        if (userDoc.exists) {
+          const userData = userDoc.data() as { username: string };
+          this.userName = userData?.username || '';
+          this.getUserPhoto(uid);
+        } else {
+          this.userName = null;
+          this.userPhoto = null;
+        }
+      },
+      (error) => {
+        console.log('Error retrieving user data:', error);
+        this.userName = null;
+        this.userPhoto = null;
+      }
+    );
+  }
+  
+  async getUserPhoto(uid: string) {
+    try {
+      const folderRef = this.storage.ref(`users/${uid}/`);
+      const files = await folderRef.listAll().toPromise();
+      
+      if (files && files.items.length > 0) {
+        const photoURL = await files.items[0].getDownloadURL();
+        this.userPhoto = photoURL;
+      } else {
+        // Si no se encuentran archivos de imagen en la carpeta, puedes asignar null a userPhoto o una URL de imagen predeterminada
+        this.userPhoto = null;
+      }
+    } catch (error) {
+      console.log('Error al recuperar la foto del usuario:', error);
+    }
+  }
+  
   logout() {
-    this.afAuth.signOut()
+    this.afAuth
+      .signOut()
       .then(() => {
         // Cierre de sesión exitoso
         this.router.navigate(['/inicioSesion']);
       })
-      .catch((error) => {
+      .catch(error => {
         // Maneja el error de cierre de sesión según tus necesidades
         console.log(error);
       });
